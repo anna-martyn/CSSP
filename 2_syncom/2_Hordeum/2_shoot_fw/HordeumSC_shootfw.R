ee0a90d461b8a49783e9d052bcf090805b18d340
anna-martyn <amartyn@dell-node-13.local>
2025-10-29 10:49:33 +0100

    added new Hordeum SC data, shootfw, rarefication

--- 2_syncom/2_Hordeum/2_shoot_fw/HordeumSC_shootfw.R
+++ 2_syncom/2_Hordeum/2_shoot_fw/HordeumSC_shootfw.R
@@ -11,18 +11,16 @@ library(multcompView)
 library(dplyr)
 
 # Load data.
-weight <- read.table("LotusSC_shootwf_input.txt", header=T, sep="\t")
-weight$Fresh_weight <- as.numeric(gsub(",", ".", weight$Fresh_weight))
+weight <- read.table("HordeumSC_shootfw.txt", header=T, sep="\t")
 
-# Set genotype factors.
+# Set colours and factor levels for genotypes.
+colors <- c("WT_uninoc"="grey","WT"="#A9C289","symrk"="#FEDA8B","ccamk"="#FDB366",
+            "nsp1"="#C0E4EF","nsp2"="#6EA6CD")
+
 weight$Genotype <- factor(
   weight$Genotype,
   levels = c("WT", "symrk", "ccamk", "nsp1", "nsp2")
 )
-
-# Set colors for genotypes.
-colors <- c("WT"="#A9C289","symrk"="#FEDA8B","ccamk"="#FDB366",
-            "nsp1"="#C0E4EF","nsp2"="#6EA6CD")
 
 # ANOVA and TukeyHSD
 aov_res <- aov(Fresh_weight ~ Genotype, data = weight)
@@ -31,7 +29,6 @@ tukey_pvals <- tukey$Genotype[, "p adj"]
 names(tukey_pvals) <- rownames(tukey$Genotype)
 letters <- multcompLetters(tukey_pvals)$Letters
 letters_df <- data.frame(Genotype = names(letters), label = letters, stringsAsFactors = FALSE)
-
 
 ## Overall ANOVA p-values and asterisks.
 p_value <- summary(aov_res)[[1]][["Pr(>F)"]][1]
@@ -88,10 +85,10 @@ p1 <- ggplot(weight, aes(x=Genotype, y=Fresh_weight, fill=Genotype)) +
     "nsp1"=expression(italic("nsp1")),
     "nsp2"=expression(italic("nsp2"))
   )) +
-  scale_y_continuous(expand=expansion(mult=c(0,0)), limits=c(0,0.08))
+  scale_y_continuous(expand=expansion(mult=c(0,0)), limits=c(0,0.8))
 
 p1
 
 # Save the plot.
-ggsave("LotusSC_shootfw.pdf", p1, width=5, height=6)
-saveRDS(p1, "LotusSC_shootfw.rds")
+ggsave("HordeumSC_shootfw.pdf", p1, width=5, height=6)
+saveRDS(p1, "HordeumSC_shootfw.rds")
