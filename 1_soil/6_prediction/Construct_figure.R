d68749ec70c4b7cdced89df65bd70689e97769a6
IbTJensen <itj@math.aau.dk>
2026-02-09 15:33:19 +0100

    Updated prediction analysis.

---
+++ 1_soil/6_prediction/Construct_figure.R
@@ -0,0 +1,262 @@
+# Load packages and set colours --------------------------
+pkg <- c(
+  "data.table", "ggplot2", "ggh4x", "ggpubr", "gridExtra",
+  "cowplot", "ggtext", "Gmisc", "glue", "htmlTable", "grid"
+)
+for(pk in pkg){
+  library(pk, character.only = T)
+}
+
+# Set working directory to source file location.
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Define colours for the genotypes, soils, and bacterial orders of interest.
+cols <- c(
+  "WT" = "#A9C289",
+  "symrk" = "#FEDA8B",
+  "ccamk" = "#FDB366",
+  "nsp1" = "#C0E4EF",
+  "nsp2" = "#6EA6CD"
+)
+
+colors <- c(NPK = "#6F944F", PK = "#B2563C", UF = "#3C7D82")
+
+order_colors <- data.frame(
+  group = c(
+    "Burkholderiales", "Caulobacterales", "Flavobacteriales", "Micrococcales",
+    "Pseudomonadales", "Rhizobiales", "Streptomycetales", "Sphingomonadales",
+    "Pseudonocardiales", "Unknown", "Other"
+  ),
+  colors = c(
+    "#645394", "#8e3563", "#05294a", "#44AAAA",
+    "#88CCAA", "#fdbb6b", "#fed5a4", "lightyellow",
+    "#95bb72", "grey", "lightgrey"
+  )
+)
+
+
+# Set working directory to source file location.
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Flowchart ----
+
+# Define a custom function to create a diamond shape, and then define the individual 
+# boxes and arrows for the flowchart.
+diamondGrob <- function(x = 0.5, y = 0.5, width = 1, height = 1, gp = gpar()) {
+  diamond_points <- matrix(
+    c(0.5, 0, 1, 0.5, 0.5, 1, 0, 0.5),
+    ncol = 2,
+    byrow = TRUE
+  )
+  polygonGrob(
+    x = diamond_points[,1],
+    y = diamond_points[,2],
+    default.units = "npc",
+    gp = gp
+  )
+}
+
+grid.newpage()
+
+train <- boxGrob(
+  "Training\n Data",
+  x = 0.55, y = 0.9,
+  txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+  box_gp = gpar(fill = "#f5fbff", col = "#4881b3", lwd = 2)
+)
+
+test <- boxGrob("Test Data",
+                x = 0.28, y = 0.9,
+                txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+                box_gp = gpar(fill = "#f5fbff", col = "#4881b3", lwd = 2))
+
+mod1 <- boxGrob("Model 1: \nPredicts NPK vs. non-NPK \nTrained on NPK, PK, and UF data",
+                x = 0.28, y = 0.65, width = unit(50, "mm"),
+                txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+                box_gp = gpar(fill = "#f5fbff", col = "#4881b3", lwd = 2))
+
+mod2 <- boxGrob("Model 2: \nPredicts PK vs. UF \nTrained on PK and UF data",
+                x = 0.78, y = 0.65, width = unit(40, "mm"),
+                txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+                box_gp = gpar(fill = "#f5fbff", col = "#4881b3", lwd = 2))
+
+pred <- boxGrob("Prediction on Test Data: \nNPK or non-NPK?",
+                x = 0.28, y = 0.45,
+                txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+                box_gp = gpar(fill = "#f5fbff", col = "#4881b3", lwd = 2))
+
+# Use diamondGrob as the box_fn in boxGrob.
+diamond_box <- boxGrob("NPK?", box_fn = diamondGrob,
+                       x = 0.28, y = 0.3,
+                       txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+                       box_gp = gpar(fill = "#f5fbff", col = "#4881b3", lwd = 2))
+
+pred2 <- boxGrob("Prediction on Test Data: \nPK or UF?",
+                 x = 0.78, y = 0.3,
+                 txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+                 box_gp = gpar(fill = "#f5fbff", col = "#4881b3", lwd = 2))
+
+concl <- boxGrob("Prediction \nconcluded",
+                 x = 0.78, y = 0.1,
+                 txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+                 box_gp = gpar(fill = "#f5fbff", col = "#4881b3", lwd = 2))
+
+concl2 <- boxGrob("Prediction \nconcluded",
+                  x = 0.28, y = 0.1,
+                  txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+                  box_gp = gpar(fill = "#f5fbff", col = "#4881b3", lwd = 2))
+
+No <- boxGrob("No",
+              x = 0.45, y = 0.3, width = 0.05,
+              txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+              box_gp = gpar(fill = "white", col = "white"))
+
+Yes <- boxGrob("Yes",
+               x = 0.28, y = 0.22, height = 0.025,
+               txt_gp = gpar(col = "black", fontsize = 8, family = "helvetica"),
+               box_gp = gpar(fill = "white", col = "white"))
+
+train; test; mod1; mod2; pred; diamond_box; pred2; concl; concl2
+
+custom_arrow <- arrow(type = "closed", length = unit(0.3, "cm"))
+
+cc1 <- connectGrob(train, mod1, "N", "l", arrow_obj = custom_arrow)
+cc2 <- connectGrob(train, mod2, "N", "l", arrow_obj = custom_arrow)
+cc3 <- connectGrob(mod1, pred, "vertical", arrow_obj = custom_arrow)
+
+cc4 <- connectGrob(test, pred, "Z", arrow_obj = custom_arrow)
+x_coords <- attr(cc4, "line")$x
+y_coords <- attr(cc4, "line")$y
+
+x_coords[2] <- x_coords[2] - unit(1, "mm")
+x_coords[3] <- x_coords[3] - unit(1, "mm")
+x_coords[4] <- coords(pred)$left + coords(pred)$width/2 + unit(1.7, "mm")
+y_coords[3] <- y_coords[3] + unit(16, "mm")
+y_coords[4] <- y_coords[4] + unit(16, "mm")
+
+new_connection <- linesGrob(x = x_coords, y = y_coords, 
+                            gp = gpar(fill = "black"))
+
+cc5 <-  connectGrob(pred, diamond_box, "vertical", arrow_obj = custom_arrow)
+cc6 <-  connectGrob(mod2, pred2, "vertical", arrow_obj = custom_arrow)
+cc7 <-  connectGrob(diamond_box, pred2, "horizontal", arrow_obj = custom_arrow,
+                    lty_gp = gpar(col = "#e81313", fill = "#e81313"))
+cc8 <-  connectGrob(diamond_box, concl2, "vertical", arrow_obj = custom_arrow,
+                    lty_gp = gpar(col = "#008a0e", fill = "#008a0e"))
+cc9 <-  connectGrob(pred2, concl, "vertical", arrow_obj = custom_arrow)
+
+cc1; cc2; cc3; grid.draw(new_connection); cc5; cc6; cc7; cc8; cc9
+No; Yes
+
+grob <- grid.grab()
+
+# # Save the flowchart.
+# saveRDS(grob, file = "LotusHordeum_Askov_prediction_flowchart.rds")
+
+Res <- fread("Prediction_results.csv")
+tax_summary <- fread("Pred_taxonomic_composition.csv")
+Ratio_amount <- fread("Pred_accuracy_summary.csv")
+
+# Prediction results plot ----
+Res[,":="(
+  Host = factor(Host, levels = c("Lotus", "Hordeum")),
+  Compartment = factor(Compartment, levels = c("Rhizosphere", "Root")),
+  Obs = factor(Obs, levels = c("NPK", "PK", "UF")),
+  Pred = factor(Pred, levels = c("UF", "PK", "NPK")),
+  Genotype = factor(Genotype, levels = c("WT", "symrk", "ccamk", "nsp1", "nsp2"))
+)]
+Res[,Prediction := Obs == Pred]
+genotype_labels_legend <- c(
+  "WT"     = "WT",
+  "symrk"  = "*symrk*",
+  "ccamk"  = "*ccamk*",
+  "nsp1"   = "*nsp1*",
+  "nsp2"   = "*nsp2*"
+)
+
+ggplot(data = Res) +
+  geom_count(aes(x = Obs, y = Pred), color = "lightgrey")+
+  scale_size_continuous(range=c(1.5,15)) +
+  geom_jitter(data = Res[Prediction == T],
+              aes(x = Obs, y = Pred, fill = Genotype),
+              position = position_jitter(width = 0.35, height = 0.35, seed = 1),
+              shape = 21, stroke = 0.25) +
+  geom_jitter(data = Res[Prediction == F],
+              aes(x = Obs, y = Pred, fill = Genotype),
+              position = position_jitter(width = 0.15, height = 0.15, seed = 1),
+              shape = 21, stroke = 0.25) +
+  scale_shape_manual(values = c(21, 21))+
+  theme_bw() +
+  labs(x = "Observed", y = "Predicted")+
+  guides(size = "none", fill = guide_legend(override.aes = list(size=3))) +
+  facet_grid(Host ~ Compartment) +
+  scale_fill_manual(values = cols, labels = genotype_labels_legend) +
+  theme(legend.position = "bottom",
+        legend.margin = margin(t = -8),
+        strip.background = element_rect(colour = NA),
+        axis.title.y = element_text(size = 8, family = "Helvetica"),
+        axis.title.x = element_text(size = 8, family = "Helvetica"),
+        axis.text.y = element_text(size = 8, family = "Helvetica",
+                                   colour = "black"),
+        axis.text.x = element_text(size = 8, family = "Helvetica",
+                                   colour = "black"),
+        legend.text = element_markdown(size = 8, family = "Helvetica",
+                                       colour = "black"),
+        legend.title = element_text(size = 8, family = "Helvetica",
+                                    colour = "black"),
+        strip.text = element_text(size = 8, family = "Helvetica",
+                                  face = "bold"),
+        legend.key.size = unit(5, "mm"))+
+  NULL -> g1; g1
+
+# Taxonomic composition plot ----
+tax_summary[!(Order %in% order_colors$group), Order:= "Other"]
+tax_summary[,":="(
+  Order = factor(Order, levels = order_colors$group),
+  Plant = factor(Plant, levels = c("Lotus", "Hordeum")),
+  Compartment = factor(Compartment, levels = c("Rhizosphere", "Root")),
+  Soil = factor(Soil, levels = c("NPK", "PK", "UF"))
+)]
+tax_summary <- tax_summary[order(Order)]
+
+ggplot(data = tax_summary, aes(x = Soil, y = RA, fill = Order))+
+  geom_bar(stat = "identity", position = "stack", linewidth = 0.1) +
+  facet_wrap2(vars(Plant, Compartment), strip = strip_nested(), nrow = 1)+
+  scale_fill_manual(
+    values = order_colors$colors,
+    breaks = order_colors$group,
+    name = "Bacterial orders"
+  )+
+  labs(x = NULL, y = "Cumulative Mean Relative Abundance")+
+  guides(fill = guide_legend(nrow = 3, title.position = "top"))+
+  scale_y_continuous(expand = c(.001, .001), limits = c(0, 0.12))+
+  theme_bw()+
+  theme(
+    legend.position = "bottom",
+    legend.box.margin = margin(0, 0, 0, -15),
+    legend.margin = margin(t = -5),
+    strip.background = element_rect(colour = NA),
+    legend.key.size = unit(0.25, 'cm'),
+    legend.key.spacing.y = unit(0, 'cm'),
+    legend.justification = c(0.75, 0),
+    axis.title.y = element_text(size = 8, family = "Helvetica"),
+    axis.text.y = element_text(size = 8, family = "Helvetica", colour = "black"),
+    axis.text.x = element_text(size = 8, family = "Helvetica", colour = "black"),
+    legend.text = element_text(size = 8, family = "Helvetica"),
+    legend.title = element_text(size = 8, family = "Helvetica"),
+    strip.text = element_text(size = 8, family = "Helvetica", face = "bold")
+  )+
+  NULL -> g2; g2
+
+#  Combining plots ----
+tg <- tableGrob(Ratio_amount, theme = ttheme_default(base_size = 8), rows = NULL)
+
+gg1 <- plot_grid(grob, tg,
+                 labels = c("A", "C"),
+                 rel_heights = c(0.8, 0.2),
+                 ncol = 1)
+gg2 <- ggarrange(g1, g2, labels = c("B", "D"), ncol = 1, heights = c(0.5, 0.5))
+gg <- ggarrange(gg1, gg2, ncol = 2)
+
+ggsave(filename = "Figure3_Askov_prediction.pdf", plot = gg,
+       width = 210, height = 200, units = "mm")
--- 1_soil/6_prediction/Prediction_analysis.R
+++ 1_soil/6_prediction/Prediction_analysis.R
@@ -1,5 +1,4 @@
 # Load packages and set colours --------------------------
-
 pkg <- c(
   "data.table", "magrittr", "ggplot2", "ggh4x", "codacore", 
   "tensorflow", "ggpubr", "gridExtra", "cowplot", "ggtext"
@@ -71,6 +70,10 @@ taxonomy_Hordeum[,c(taxa_levels):= tstrsplit(Taxon, "; ", fill = "u__Unknown")]
 taxonomy_Hordeum[,c(taxa_levels):= lapply(.SD, substr, 4, 1000), .SDcols = taxa_levels]
 taxonomy_Hordeum[,Taxon:=NULL]
 setcolorder(taxonomy_Hordeum, c("Feature ID", taxa_levels, "Confidence"))
+
+# # Remove non-bacterial ASVs from 
+Non_Bac_ASV <- taxonomy_Lotus[Kingdom != "Bacteria", "Feature ID"][[1]]
+ASV_table_Lotus <- ASV_table_Lotus[!(ASVid %in% Non_Bac_ASV)]
 
 # Set up list for easy retrieval later
 ASV_table <- list(Lotus = ASV_table_Lotus, Hordeum = ASV_table_Hordeum)
@@ -232,7 +235,8 @@ for(i in 1:nrow(Opt)){
     ),
     ncol = 2
   )
-  NPK_ASVs_in_Ratios <- apply(Mat_temp, 1, function(x) paste(x[1], x[2], sep = "/"))
+  # NPK_ASVs_in_Ratios <- apply(Mat_temp, 1, function(x) paste(x[1], x[2], sep = "/"))
+  NPK_ASVs_in_Ratios <- rowSums(Mat_temp)
   NPK_ratios <- paste0(
     length(num_vec_NPK), " (",
     paste(NPK_ASVs_in_Ratios, collapse = ", "), ")"
@@ -245,7 +249,8 @@ for(i in 1:nrow(Opt)){
     ),
     ncol = 2
   )
-  PK_ASVs_in_Ratios <- apply(Mat_temp, 1, function(x) paste(x[1], x[2], sep = "/"))
+  # PK_ASVs_in_Ratios <- apply(Mat_temp, 1, function(x) paste(x[1], x[2], sep = "/"))
+  PK_ASVs_in_Ratios <- rowSums(Mat_temp)
   PK_ratios <- paste0(
     length(num_vec_PK), " (",
     paste(PK_ASVs_in_Ratios, collapse = ", "), ")"
@@ -293,6 +298,19 @@ for(i in 1:nrow(Opt)){
   lst <- c(Sum_num_NPK, Sum_denom_NPK, Sum_num_PK, Sum_denom_PK)
   Ratio_summary_temp <- rbindlist(lst)
   Ratio_summary_temp[,":="(Compartment = Opt$Compartment[i], Host = Opt$Host[i])]
+  pred_ASVs <- unique(Ratio_summary_temp$ASVid)
+  
+  ASV_table_RA_sub <- ASV_table_RA[[Opt$Host[i]]]
+  ASV_table_RA_sub <- ASV_table_RA_sub[ASVid %in% pred_ASVs]
+  ASV_table_RA_sub <- transpose(
+    ASV_table_RA_sub, keep.names = "SampleID", make.names = "ASVid"
+  )
+  ASV_table_RA_sub <- merge(meta_data_sub, ASV_table_RA_sub, by = "SampleID")
+  mean_RAs <- ASV_table_RA_sub[,lapply(.SD, mean), Soil, .SDcols = pred_ASVs]
+  mean_RAs <- transpose(mean_RAs, make.names = "Soil", keep.names = "ASVid")
+  colnames(mean_RAs)[-1] <- paste(colnames(mean_RAs)[-1], "RA", sep = "_")
+
+  Ratio_summary_temp <- merge(Ratio_summary_temp, mean_RAs)
   Ratio_summary_list[[i]] <- Ratio_summary_temp
 
   # Summarising RA and taxonomic information for ASVs used for estimation
@@ -317,94 +335,21 @@ for(i in 1:nrow(Opt)){
 
 tax_summary <- rbindlist(tax_summary_list)
 tax_summary[,.(RA = sum(RA)), .(Plant, Compartment, Soil)]
+fwrite(tax_summary, "Pred_taxonomic_composition.csv")
 
 Res <- rbindlist(Res_list)
 Ratio_summary <- rbindlist(Ratio_summary_list)
 Ratio_summary <- merge(Ratio_summary, taxonomy, by = "ASVid")
+Ratio_summary <- Ratio_summary[order(Host, Compartment, Prediction, Role)]
 fwrite(Ratio_summary, file = "LotusHordeum_Askov_prediction_ratios_summary.csv")
 
 Ratio_amount <- rbindlist(Ratio_amount_list)
+Ratio_amount[,":="(
+  Accuracy = paste0(round(Accuracy*100, 1), "%"),
+  Compartment = relevel(Compartment, ref = "Rhizosphere")
+)]
+Ratio_amount <- Ratio_amount[order(Host, Compartment)]
+fwrite(Ratio_amount, "Pred_accuracy_summary.csv")
 
 Res[,.(Accuracy = mean(Obs == Pred)), .(Host, Compartment)]
-
-# Visualising results ------------------------------------------------
-
-# Prediction results
-Res[,":="(
-  Host = factor(Host, levels = c("Lotus", "Hordeum")),
-  Compartment = factor(Compartment, levels = c("Rhizosphere", "Root")),
-  Obs = factor(Obs, levels = c("NPK", "PK", "UF")),
-  Pred = factor(Pred, levels = c("UF", "PK", "NPK"))
-)]
-Res[,Prediction := Obs == Pred]
-genotype_labels_legend <- c(
-  "WT"     = "WT",
-  "symrk"  = "*symrk*",
-  "ccamk"  = "*ccamk*",
-  "nsp1"   = "*nsp1*",
-  "nsp2"   = "*nsp2*"
-)
-
-ggplot(data = Res) +
-  geom_count(aes(x = Obs, y = Pred), color = "lightgrey")+
-  scale_size_continuous(range=c(1.5,15)) +
-  geom_jitter(data = Res[Prediction == T],
-              aes(x = Obs, y = Pred, fill = Genotype),
-              position = position_jitter(width = 0.35, height = 0.35, seed = 1),
-              shape = 21, stroke = 0.25) +
-  geom_jitter(data = Res[Prediction == F],
-              aes(x = Obs, y = Pred, fill = Genotype),
-              position = position_jitter(width = 0.15, height = 0.15, seed = 1),
-              shape = 21, stroke = 0.25) +
-  scale_shape_manual(values = c(21, 21))+
-  theme_bw() +
-  labs(x = "Observed", y = "Predicted")+
-  guides(size = "none", fill = guide_legend(override.aes = list(size=3))) +
-  facet_grid(Host ~ Compartment) +
-  scale_fill_manual(values = cols, labels = genotype_labels_legend) +
-  theme(legend.position = "bottom",
-        legend.margin = margin(t = -8),
-        strip.background = element_rect(colour = NA),
-        axis.title.y = element_text(size = 8, family = "Helvetica"),
-        axis.title.x = element_text(size = 8, family = "Helvetica"),
-        axis.text.y = element_text(size = 8, family = "Helvetica",
-                                   colour = "black"),
-        axis.text.x = element_text(size = 8, family = "Helvetica",
-                                   colour = "black"),
-        legend.text = element_markdown(size = 8, family = "Helvetica",
-                                       colour = "black"),
-        legend.title = element_text(size = 8, family = "Helvetica",
-                                    colour = "black"),
-        strip.text = element_text(size = 8, family = "Helvetica",
-                                  face = "bold"),
-        legend.key.size = unit(5, "mm"))+
-  NULL -> g1; g1
-
-# Taxonomic composition
-tax_summary[!(Order %in% order_colors$group), Order:= "Other"]
-ggplot(data = tax_summary, aes(x = Soil, y = RA, fill = Order))+
-  geom_bar(stat = "identity", position = "stack", linewidth = 0.1) +
-  facet_wrap2(vars(Plant, Compartment), strip = strip_nested(), nrow = 1)+
-  scale_fill_manual(values = order_colors$colors, breaks = order_colors$group,
-                    name = "Bacterial orders")+
-  labs(x = NULL, y = "Cumulative Mean Relative Abundance")+
-  guides(fill = guide_legend(nrow = 4, title.position = "top"))+
-  scale_y_continuous(expand = c(.001, .001), limits = c(0, 0.2))+
-  theme_bw()+
-  theme(legend.position = "bottom",
-        legend.box.margin = margin(0, 0, 0, -15),
-        legend.margin = margin(t = -5),
-        strip.background = element_rect(colour = NA),
-        legend.key.size = unit(0.25, 'cm'),
-        legend.key.spacing.y = unit(0, 'cm'),
-        legend.justification = c(0.75, 0),
-        axis.title.y = element_text(size = 8, family = "Helvetica"),
-        axis.text.y = element_text(size = 8, family = "Helvetica",
-                                   colour = "black"),
-        axis.text.x = element_text(size = 8, family = "Helvetica",
-                                   colour = "black"),
-        legend.text = element_text(size = 8, family = "Helvetica"),
-        legend.title = element_text(size = 8, family = "Helvetica"),
-        strip.text = element_text(size = 8, family = "Helvetica",
-                                  face = "bold"))+
-  NULL -> g2; g2
+fwrite(Res, "Prediction_results.csv")
